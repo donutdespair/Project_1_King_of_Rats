1b6952882496540b01745caaab7f546ce0500d77
carson hyde <carsonmhyde@gmail.com>
2016-11-08 07:17:21 -0500

    final commit

--- ratscript.js
+++ ratscript.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
 console.log('script loaded');
 
 /////////////Game Sounds///////////
-var gameTheme = new Audio('gameaudio.mp3');
-var smashSound = new Audio('smash.mp3');
+var gameTheme = new Audio('media/gameaudio.mp3');
+var smashSound = new Audio('media/smash.mp3');
 
 ////////////Global Variables///////////
 var score = 0;
@@ -12,101 +12,100 @@ var timer = 60;
 var body = $('body');
 var display = $(".display");
 var rat = $(".rat");
-var intervalId;
-var timeoutId;
+var gameInterval;
 var createInterval;
 var ratTimeout;
 
 ///////////Start Game//////////
 $(function() {
-$('.scoreboard').hide();
-$('.rat').hide();
-$('.display').hide();
-$('.hole').hide();
-//everything is hidden until start button is clicked
+  $('.scoreboard').hide();
+  $('.rat').hide();
+  $('.display').hide();
+  //everything is hidden until start button is clicked
 $("#startbutton" ).click(function() {
-$(this).hide();
-//starts button hidden onclick, game shows, audio starts
-$("#startscreen" ).hide();
-$('.display').show();
-$('.hole').show();
-$('.scoreboard').show();
-$('#scorenum').text(" 0");
-gameTheme.play();
-intervalId = setInterval(gameTime, 1000);
-createInterval = setInterval(createPests, 1000);
-ratTimeout = setTimeout(ratOut, 500);
+  $(this).hide();
+  //starts button hidden onclick, game shows, audio starts
+  $("#startscreen" ).hide();
+  $('.display').show();
+  $('.scoreboard').show();
+  $('#scorenum').text(" 0");
+  gameTheme.play();
+  gameInterval = setInterval(gameTime, 1000);
+  createInterval = setInterval(createPests, 800);
+  ratTimeout = setTimeout(ratOut, 900);
     });
 
 
 
 /////////// Rats Created///////////////
 var createPests = function(){
-var randomRat = Math.floor(Math.random() * $('.rat').length);
-//this chooses the number of the rat div to show
-//you have a random number between 0 and 1 * the number
-//of "rat" divs that is rounded down to the nearest whole number
-$('.rat').hide().eq(randomRat).show().effect("shake");
+  var randomRat = Math.floor(Math.random() * $('.rat').length);
+  //this chooses the number of the rat div to show
+  //you have a random number between 0 and 1 * the number
+  //of "rat" divs that is rounded down to the nearest whole number
+  $('.rat').hide().eq(randomRat).show().removeAttr('style').effect('bounce');
     };
-//the random value is passed to the .eq method with constructs
-//a new jQuery object based on the element passed to it (the hidden rats)
-//the randomly chosen rat is shown and shakes
-//a new rat is created every 600ms
-setInterval(createPests, 1000);
+  //the random value is passed to the .eq method with constructs
+  //a new jQuery object based on the element passed to it (the hidden rats)
+  //the randomly chosen rat is shown and bounces a little
+
 
 
 
 ///////////// Rats Disappear/////////////
 var ratOut = function() {
-$(".rat").fadeOut().hide();
+  $(".rat").hide();
   }
-setTimeout(ratOut, 500);
-//this function causes the rat to fade out and hide at 700ms after showing
+//rat hides after time specified in setTimeout above
 
 //////////Rats get SMAAAASHED///////
  $(".rat").on('click', theScore)
  //rat div on click event
 function theScore() {
-  score = score +1;
-  //a point is added for each rat clicked
-  //console.log(score); //for testing
-  $('#scorenum').text(" " + score);
-  //the score text is added to the scoreboard
-  $(this).addClass("smash");
-  //the rat gets the class "smash" which changes it to a pool of rat remains
-  smashSound.play();
-//the splat sound plays
-var smashGone =
-setTimeout(function() {
-$(".rat").removeClass('smash');
-//the rats lose the smash class and hide so they can be shown again
-$(".rat").hide();
-      }, 500)
+    score = score +1;
+    //a point is added for each rat clicked
+    //console.log(score); //for testing
+   $('#scorenum').text(" " + score);
+    //the score text is added to the scoreboard
+    $(this).css('background-image', 'url(media/blood.png)');
+    smashSound.play()
+  //the splat sound plays
+  setTimeout(function() {
+  //the rats lhide so they can be shown again
+  //removes the added css attr that changes the background to the splatter
+  $(this).hide();
+      }, 300)
 };
 
 var gameTime = function () {
 timer--;
 //timer counts down by one
 $("#timernum").text(' ' + timer);
+//prints time to timer on scoreboard
 if (timer == 0) {
-    alert('Times up!');
-    clearInterval(intervalId);
-    clearTimeout(timeoutId);
+  //clears timer, rat genration, and rat disappearance
+    clearInterval(gameInterval);
     clearInterval(createInterval);
     clearTimeout(ratTimeout);
-      if (score < 50) {
-      alert("You lose")
+//conditions for winning and losing
+      if (score < 25) {
+          if (window.confirm("You lose, play again?")) {
+              location.reload();
+            }
+          //user can confirm to play again or cancel and be redirected
+          else (document.location.href = "http://www.aspca.org/nyc");
           }
-      else alert("You win")
+      else {
+          if (window.confirm("You win, play again?")) {
+               location.reload();
+            }
+          else (document.location.href = "http://www.aspca.org/nyc");
+            }
         }
 
     };
-
-
-
-
-  });
-    });
+});
+});
 
 
 ///////Game Audio/////
@@ -115,20 +114,10 @@ if (timer == 0) {
 //http://www.tannerhelland.com/9/clowns/
 
 /////Sounds/////
-//smash --> http://soundbible.com/1945-Smashing.html
+//smash --> http://soundbible.com/1876-Punch-HD.html
 
 
 //////Programming Sources Consulted//////
 //https://api.jquery.com/eq/
-//http://stackoverflow.com/questions/12569879/create-an-animation-sequence-loop-jquery
 //http://stackoverflow.com/questions/19244394/creating-a-timer-for-a-javascript-game-gives-undesirable-results
-//https://api.jqueryui.com/shake-effect/
 //http://ejohn.org/blog/how-javascript-timers-work/
-
-
-
-
-
-
-
-
